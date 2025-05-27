d3c1997814e6b676663ec3446219f381168def28
Alberto Colella <162927029+Abertocoea@users.noreply.github.com>
2025-05-27 11:55:38 +0200

    Update Losses.py con SCE

--- src/Losses.py
+++ src/Losses.py
@@ -19,3 +19,22 @@ class GCODLoss(torch.nn.Module):
 
         loss = weight * ce_loss
         return loss.mean()
+
+class SCELoss(torch.nn.Module):
+    def __init__(self, alpha=0.7, beta=0.3, num_classes=6):
+        super(SCELoss, self).__init__()
+        self.alpha = alpha
+        self.beta = beta
+        self.ce = torch.nn.CrossEntropyLoss()
+        self.num_classes = num_classes
+
+    def forward(self, logits, targets):
+        ce_loss = self.ce(logits, targets)
+
+        probs = torch.softmax(logits, dim=1)
+        targets_onehot = torch.nn.functional.one_hot(targets, num_classes=self.num_classes).float()
+        targets_onehot = torch.clamp(targets_onehot, min=1e-4, max=1.0)  # evita log(0)
+
+        rce_loss = (-probs * torch.log(targets_onehot)).sum(dim=1).mean()
+
+        return self.alpha * ce_loss + self.beta * rce_loss
